73fb8e69096d5bd1fb96b14b083f372566c4fcb8
Danik <95442609+BanduraDanik1@users.noreply.github.com>
2022-12-01 19:19:33 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,15 @@ import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
-const inputEl = document.querySelector('#datetime-picker')
-flatpickr(inputEl, {enableTime: true,
-    dateFormat: "Y-m-d H:i",});
+const inputEl = document.querySelector('#datetime-picker');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+flatpickr(inputEl, options);
