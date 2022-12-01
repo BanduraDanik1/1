d04a5a0458aaabe241dd1598e4c56b9d3df6568d
Danik <95442609+BanduraDanik1@users.noreply.github.com>
2022-12-01 18:49:16 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const bodyEl = document.querySelector('body')
+const ref = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+}
 
+ref.startBtn.addEventListener('click', onStartBtn);
+ref.stopBtn.addEventListener('click', onStopBtn);
+let intervalId = null;
+let isActive = false;
+
+function onStartBtn (e) {
+    if(isActive) {
+        return
+    }
+    isActive = true;
+    console.log('h')
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+        console.log(`Color - ${bodyEl.style.backgroundColor}`)
+    }, 1000);
+}
+
+function onStopBtn (e) {
+    clearInterval(intervalId)
+}
