2d31d77bdf1ac68b303e57b1e02e69f8981ddcf7
BanduraDanik1 <95442609+BanduraDanik1@users.noreply.github.com>
2022-12-01 16:49:43 +0000

    Deploying to gh-pages from @ BanduraDanik1/goit-js-hw-09@d04a5a0458aaabe241dd1598e4c56b9d3df6568d 🚀

---
+++ 01-color-switcher.5ce155ca.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),o={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};o.startBtn.addEventListener("click",(function(o){if(e)return;e=!0,console.log("h"),n=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),console.log("Color - ".concat(t.style.backgroundColor))}),1e3)})),o.stopBtn.addEventListener("click",(function(t){clearInterval(n)}));var n=null,e=!1}();
+//# sourceMappingURL=01-color-switcher.5ce155ca.js.map
